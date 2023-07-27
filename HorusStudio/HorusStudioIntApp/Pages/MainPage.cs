bc842c8dc6cf8ee6f623de093409b363badc011e
adospace <adospace@msn.com>
2023-07-27 19:10:26 +0200

    HorusStudio

---
+++ HorusStudio/HorusStudioIntApp/Pages/MainPage.cs
@@ -0,0 +1,38 @@
+﻿using Maui.Skeleton;
+using Maui.Skeleton.Animations;
+using MauiReactor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorusStudioIntApp.Pages;
+
+internal class MainPageState
+{
+    public bool IsBusy { get; set; }
+}
+
+internal class MainPage : Component<MainPageState>
+{
+    public override VisualNode Render()
+    {
+        return new ContentPage
+        {
+            new VStack(spacing: 5)
+            {
+                new Button("Toggle", () => SetState(s => s.IsBusy = !s.IsBusy)),
+
+                new Frame
+                {
+                    new Label("Label")
+                }
+                .BackgroundColor(Colors.Transparent)
+                .Set(Skeleton.IsBusyProperty, State.IsBusy)
+                .Set(Skeleton.BackgroundColorProperty, Color.FromArgb("#c6c6c5"))
+                .Set(Skeleton.AnimationProperty, new FadeAnimation(500, null))
+            }
+        };
+    }
+}
---
+++ HorusStudio/HorusStudioIntApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HorusStudioIntApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
